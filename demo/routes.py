b4b8cb629cb02ba3d8a0cadc8f873dcfd15bc0ed
Martonos Magor <martonosmagor@yahoo.com>
2021-08-19 13:56:20 +0300

    adding route

--- demo/routes.py
+++ demo/routes.py
@@ -1,3 +1,8 @@
 from flask import render_template, url_for, flash, redirect
 from demo import app
 from demo.models import User, Post
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
